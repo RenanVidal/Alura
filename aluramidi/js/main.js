fab4f795af7f475d05abdbcbf10d55f9f8cd23fd
RenanVidal <rvidal.rvr@gmail.com>
2023-01-30 17:52:30 -0300

    Curso Javascript para Web - aula 4 finalizada

--- aluramidi/js/main.js
+++ aluramidi/js/main.js
@@ -1,6 +1,18 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
-} 
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+} 
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    };
+
+}
