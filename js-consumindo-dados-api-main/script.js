cf9f60897e9125548bbb6d769a95d5f9bbe79c91
RenanVidal <rvidal.rvr@gmail.com>
2023-02-24 16:54:07 -0300

    Projeto pagina de cadastro da AluraBooks - Curso JavaScript: consumindo e tratando dados de uma API - Iniciado

---
+++ js-consumindo-dados-api-main/script.js
@@ -0,0 +1,11 @@
+console.log('mandando oi pro amigo!');
+
+function mandaMensagem () {
+    console.log('Tudo bem?');
+    console.log('Vou te mandar uma solicitação!');
+    console.log('Solicitação recebida!')
+};
+
+setTimeout(mandaMensagem, 5000);
+
+console.log('Tchau Tchau');
