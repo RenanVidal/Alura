b481077081067287c81242a32c8f71320c09bee3
RenanVidal <rvidal.rvr@gmail.com>
2023-02-27 16:53:25 -0300

    Projeto pagina de cadastro da AluraBooks - Curso JavaScript: consumindo e tratando dados de uma API -

--- js-consumindo-dados-api-main/script.js
+++ js-consumindo-dados-api-main/script.js
@@ -1,11 +1,2 @@
-console.log('mandando oi pro amigo!');
+const consultaCep = fetch ('viacep.com.br/ws/01001000/json/');
 
-function mandaMensagem () {
-    console.log('Tudo bem?');
-    console.log('Vou te mandar uma solicitação!');
-    console.log('Solicitação recebida!')
-};
-
-setTimeout(mandaMensagem, 5000);
-
-console.log('Tchau Tchau');
