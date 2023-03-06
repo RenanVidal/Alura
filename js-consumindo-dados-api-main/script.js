2b1c60fad6534ee0ce544bfe43f65b03df0de329
RenanVidal <rvidal.rvr@gmail.com>
2023-03-06 18:00:00 -0300

    Projeto pagina de cadastro da AluraBooks - Curso JavaScript: consumindo e tratando dados de uma API - att

--- js-consumindo-dados-api-main/script.js
+++ js-consumindo-dados-api-main/script.js
@@ -1,14 +1,19 @@
-async function buscaEndereco () {
+async function buscaEndereco (cep) {
     try {
-        const consultaCep = await fetch (`https://viacep.com.br/ws/01001000/json/`);
+        const consultaCep = await fetch (`https://viacep.com.br/ws/${cep}/json/`);
         const consultaCepConvertida = await consultaCep.json();
         if (consultaCepConvertida.erro) {
             throw Error ('Cep não existente!');
-        }
+        };
+        const cidade = document.getElementById('cidade');
+        const logadouro = document.getElementById('endereco');
+        const estado = document.getElementById('estado');
         console.log(consultaCepConvertida);
+        return consultaCepConvertida;
     } catch (erro) {
         console.log(erro);
     };
 }
 
-buscaEndereco();
+const cep = document.getElementById('cep');
+cep.addEventListener("focusout", () => buscaEndereco(cep.value));
