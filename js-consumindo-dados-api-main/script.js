00d5f544896760482cf97262fe54e563e1be6309
RenanVidal <rvidal.rvr@gmail.com>
2023-03-08 17:12:56 -0300

    Projeto pagina de cadastro da AluraBooks - Curso JavaScript: consumindo e tratando dados de uma API - Finalizado

--- js-consumindo-dados-api-main/script.js
+++ js-consumindo-dados-api-main/script.js
@@ -1,16 +1,28 @@
 async function buscaEndereco (cep) {
+    const mensagemErro = document.getElementById('erro'); 
+    mensagemErro.innerHTML = "";
+
     try {
         const consultaCep = await fetch (`https://viacep.com.br/ws/${cep}/json/`);
         const consultaCepConvertida = await consultaCep.json();
         if (consultaCepConvertida.erro) {
             throw Error ('Cep não existente!');
         };
+
         const cidade = document.getElementById('cidade');
         const logadouro = document.getElementById('endereco');
         const estado = document.getElementById('estado');
+        const bairro = document.getElementById('bairro');
+
+        cidade.value = consultaCepConvertida.localidade;
+        logadouro.value = consultaCepConvertida.logadouro;
+        estado.value = consultaCepConvertida.uf;
+        bairro.value = consultaCepConvertida.bairro;
+
         console.log(consultaCepConvertida);
         return consultaCepConvertida;
     } catch (erro) {
+        mensagemErro.innerHTML = '<p>CEP inválido. Tente novamente!</p>';
         console.log(erro);
     };
 }
