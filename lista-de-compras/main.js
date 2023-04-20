ca97e86e60561c9b17f83451ff91330e39516815
Renan Vidal <rvidal.rvr@gmail.com>
2023-04-19 22:11:46 -0300

    Atualizações

--- lista-de-compras/main.js
+++ lista-de-compras/main.js
@@ -2,13 +2,42 @@ let listaDeItens = [];
 
 const form = document.getElementById('form-itens');
 const itensInput = document.getElementById('receber-item');
+const ulItens = document.getElementById('lista-de-itens');
 
 form.addEventListener('submit', function (evento) {
     evento.preventDefault();
     salvarItem();
+    mostrarItem();
 })
 
 function salvarItem() {
-    const comprasItem = itensInput.ariaValueMax;
+    const comprasItem = itensInput.value;
+    const checarDuplicado = listaDeItens.some((elemento) =>  elemento.valor.toUpperCase() === comprasItem.toUpperCase())
 
-    listaDeItens
+    if(checarDuplicado) {
+        alert("Item já existe");
+    } else {
+        listaDeItens.push({
+            valor:comprasItem,
+        });
+    }
+
+    console.log(listaDeItens);
+}
+
+function mostrarItem() {
+    ulItens.innerHTML = ''
+    listaDeItens.forEach((elemento, index) => {
+        ulItens.innerHTML += `
+            <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
+                <div>
+                    <input type="checkbox" class="is-clickable" />
+                    <input type="text" class="is-size-5" value="${elemento.valor}"></input>
+                </div>
+                <div>
+                    <i class="fa-solid fa-trash is-clickable deletar"></i>
+                </div>
+            </li>
+        `
+    })
+}
