d82164f200a6f2fb11208b74f325028a25591e9e
RenanVidal <rvidal.rvr@gmail.com>
2023-05-02 17:33:21 -0300

    ByreBank - Att

--- bytebank/script/imprimeCotacao.js
+++ bytebank/script/imprimeCotacao.js
@@ -1,16 +1,28 @@
-const lista = document.querySelector('[data-lista');
+const lista = document.querySelectorAll('[data-lista');
 
-function imprimeCotacao(nome, valor) {
+function selecionaCotacao(nome, valor) {
+    lista.forEach((listaEscolhida) => {
+        if(listaEscolhida.id == nome) {
+            imprimeCotacao(listaEscolhida, nome, valor);
+        }
+    });
+}
+
+function imprimeCotacao(lista, nome, valor) {
     lista.innerHTML = '';
-
+    const plurais = {
+        'dolar': 'dolares',
+        'iene': 'ienes', 
+        'euro': 'euros'
+    }
     for(let mutiplicador = 1; mutiplicador <= 1000; mutiplicador *=10 ) {
         const listaItem =document.createElement('li');
         listaItem.innerHTML =
         `
-            ${mutiplicador} ${nome}: R$${(valor * mutiplicador).toFixed(2)}
+            ${mutiplicador} ${mutiplicador == 1 ? nome : plurais[nome]}: R$${(valor * mutiplicador).toFixed(2)}
         `
         lista.appendChild(listaItem);
     }
 }
 
-export default imprimeCotacao;
+export default selecionaCotacao;
--- bytebank/script/script.js
+++ bytebank/script/script.js
@@ -1,7 +1,6 @@
-import imprimeCotacao from "./imprimeCotacao.js";
+import selecionaCotacao from "./imprimeCotacao.js";
 
 const graficoDolar = document.getElementById('graficoDolar');
-
 const graficoParaDolar = new Chart(graficoDolar, {
     type: 'line',
     data: {
@@ -13,16 +12,6 @@ const graficoParaDolar = new Chart(graficoDolar, {
         }]
     },
 });
-
-setInterval(() => conectaAPI(), 5000);
-async function conectaAPI() {
-    const conecta = await fetch("https://economia.awesomeapi.com.br/json/last/USD-BRL");
-    const conectaTraduzido = await conecta.json();
-    let tempo = geraHorariro();
-    let valor = conectaTraduzido.USDBRL.ask;
-    adicionarDados(graficoParaDolar, tempo, valor);
-    imprimeCotacao('dolar', valor);
-}
 
 function geraHorariro() {
     let data = new Date();
@@ -38,3 +27,57 @@ function adicionarDados(grafico, legenda, dados) {
     });
     grafico.update();
 }
+
+let workerDolar = new Worker('/script/workers/workerDolar.js');
+workerDolar.postMessage('usd');
+
+workerDolar.addEventListener("message", event => {
+    let tempo = geraHorariro();
+    let valor = event.data.ask;
+    selecionaCotacao('dolar', valor);
+    adicionarDados(graficoParaDolar, tempo,valor);
+});
+
+const graficoIene = document.getElementById('graficoIene');
+const graficoParaIene = new Chart(graficoIene,  {
+    type: 'line',
+    data: {
+        labels: [],
+        datasets: [{
+            label: 'Iene',
+            data: [],
+            borderWidth: 1
+        }]
+    }, 
+});
+
+let workerIene = new Worker('/script/workers/workerIene.js');
+workerIene.postMessage('iene');
+workerIene.addEventListener('message', event => {
+    let tempo = geraHorariro();
+    let valor = event.data.ask;
+    selecionaCotacao('iene', valor);
+    adicionarDados(graficoParaIene, tempo,valor);
+});
+
+const graficoEuro = document.getElementById('graficoEuro');
+const graficoParaEuro = new Chart(graficoEuro,  {
+    type: 'line',
+    data: {
+        labels: [],
+        datasets: [{
+            label: 'Euro',
+            data: [],
+            borderWidth: 1
+        }]
+    }, 
+});
+
+let workerEuro = new Worker('/script/workers/workerEuro.js');
+workerEuro.postMessage('euro');
+workerEuro.addEventListener('message', event => {
+    let tempo = geraHorariro();
+    let valor = event.data.ask;
+    selecionaCotacao('euro', valor);
+    adicionarDados(graficoParaEuro, tempo,valor);
+});
